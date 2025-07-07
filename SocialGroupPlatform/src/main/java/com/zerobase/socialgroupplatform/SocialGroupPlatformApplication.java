90adac37a142e170141b0ce5970b7678f6bc210b
Arti-39 <tysc1412@naver.com>
2025-07-07 23:49:50 +0900

    Add files via upload

---
+++ SocialGroupPlatform/src/main/java/com/zerobase/socialgroupplatform/SocialGroupPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.socialgroupplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialGroupPlatformApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SocialGroupPlatformApplication.class, args);
+  }
+
+}
---
+++ SocialGroupPlatform/src/test/java/com/zerobase/socialgroupplatform/SocialGroupPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.socialgroupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialGroupPlatformApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
